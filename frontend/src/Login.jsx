6c2c80f8c2b50483f4a8911489f843ceef219fd3
alaa <aedalaa36@gmail.com>
2024-03-29 07:21:38 +0100

    add the routes

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -8,18 +8,18 @@ function Login() {
         <form action="">
             {/**create input email  */}
             <div className="mb-3">
-                <label htmlFor="email">Email</label>
-                <input type="email" placeholder="Enter Email"/>
+                <label htmlFor="email"><strong>Email</strong></label>
+                <input type="email" placeholder="Enter Email" className="form-control rounded-0"/>
             </div>
                {/**create input password  */}
                <div className="mb-3">
-                <label htmlFor="password">Password</label>
-                <input type="password" placeholder="Enter Password"/>
+                <label htmlFor="password"><strong>Password</strong></label>
+                <input type="password" placeholder="Enter Password" className="form-control rounded-0"/>
             </div>
-            <button className="btn btn-success">Log in</button>
+            <button className="btn btn-success w-100 rounded-0"><strong>Log in</strong></button>
             {/**creat account if you don't have an account  */}
             <p>You are agree to aour terms and policies</p>
-            <button className="btn btn-default border">Create Account</button>
+            <button className="btn btn-default border w-100 bg-light rounded-0"><strong>Create Account</strong></button>
         </form>
       </div>
     </div>
