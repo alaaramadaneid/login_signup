b073a8c8af6fa0e01459f8d3dd895aae642949f7
alaa <aedalaa36@gmail.com>
2024-03-29 08:31:53 +0100

    create page validation input

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Validation from './LoginValidation'
+
+
+
 
 function Login() {
+    const [values, setValues] = useState({
+        email: '',
+        password:''
+    })
+    const[errors, setErrors] = useState({})
+
+    const handleInput = (event) =>{
+            setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+    }
+   //create function handlSubmit 
+
+   const handleSubmit =(event) =>{
+    event.preventDefault();
+    setErrors(Validation(values))
+
+   }
+
+
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-25">
+        <h2 className="text-center">Sign-In</h2>
         {/** start create the form login in  */}
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
             {/**create input email  */}
             <div className="mb-3">
                 <label htmlFor="email"><strong>Email</strong></label>
-                <input type="email" placeholder="Enter Email" className="form-control rounded-0"/>
+                <input type="email" onChange={handleInput} name="email" placeholder="Enter Email" className="form-control rounded-0"/>
+                <span className="text-danger">{errors.email}</span>
             </div>
                {/**create input password  */}
                <div className="mb-3">
                 <label htmlFor="password"><strong>Password</strong></label>
-                <input type="password" placeholder="Enter Password" className="form-control rounded-0"/>
+                <input type="password" onChange={handleInput} name="password" placeholder="Enter Password" className="form-control rounded-0"/>
+                <span className="text-danger">{errors.password}</span>
+
             </div>
-            <button className="btn btn-success w-100 rounded-0"><strong>Log in</strong></button>
+            <button type='submit' className="btn btn-success w-100 rounded-0"><strong>Log in</strong></button>
             {/**creat account if you don't have an account  */}
             <p>You are agree to aour terms and policies</p>
-            <button className="btn btn-default border w-100 bg-light rounded-0"><strong>Create Account</strong></button>
+            <Link to="/signup" className="btn btn-default border w-100 bg-light rounded-0 test-decoration-none" ><strong>Create Account</strong></Link>
         </form>
       </div>
     </div>
--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div>
-      
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2 className="text-center">Sign-Up</h2>
+        {/** start create the form login in  */}
+        <form action="">
+            {/**create input email  */}
+            <div className="mb-3">
+                <label htmlFor="email"><strong>Email</strong></label>
+                <input type="email" placeholder="Enter Email" className="form-control rounded-0"/>
+            </div>
+            {/**create input name */}
+            <div className="mb-3">
+                <label htmlFor="name"><strong>Name</strong></label>
+                <input type="text" placeholder="Enter Name" className="form-control rounded-0"/>
+            </div>
+               {/**create input password  */}
+               <div className="mb-3">
+                <label htmlFor="password"><strong>Password</strong></label>
+                <input type="password" placeholder="Enter Password" className="form-control rounded-0"/>
+            </div>
+            <button className="btn btn-success w-100 rounded-0"><strong>Signup</strong></button>
+            {/**creat account if you don't have an account  */}
+            <p>You are agree to aour terms and policies</p>
+            <Link to="/" className="btn btn-default border w-100 bg-light rounded-0 test-decoration-none" ><strong>Login</strong></Link>
+        </form>
+      </div>
     </div>
   )
 }
