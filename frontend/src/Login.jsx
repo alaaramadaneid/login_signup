9ad0cc50b619e9b77ed57990c39066e28266a8bf
alaa <aedalaa36@gmail.com>
2024-03-29 06:59:10 +0100

    create login page wethe tow inputs email and password

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -3,7 +3,21 @@ import React from 'react'
 function Login() {
   return (
     <div>
-      
+      <div>
+        {/** start create the form login in  */}
+        <form action="">
+            {/**create input email  */}
+            <div className="mb-3">
+                <label htmlFor="email">Email</label>
+                <input type="email" placeholder="Enter Email"/>
+            </div>
+               {/**create input password  */}
+               <div className="mb-3">
+                <label htmlFor="password">Password</label>
+                <input type="password" placeholder="Enter Password"/>
+            </div>
+        </form>
+      </div>
     </div>
   )
 }
