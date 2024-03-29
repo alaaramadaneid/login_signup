45ed80928b3640a66a4c17b10d00ea9a14f24ded
alaa <aedalaa36@gmail.com>
2024-03-29 07:07:45 +0100

    add the style from bootstrap

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
-      <div>
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
         {/** start create the form login in  */}
         <form action="">
             {/**create input email  */}
@@ -16,6 +16,10 @@ function Login() {
                 <label htmlFor="password">Password</label>
                 <input type="password" placeholder="Enter Password"/>
             </div>
+            <button className="btn btn-success">Log in</button>
+            {/**creat account if you don't have an account  */}
+            <p>You are agree to aour terms and policies</p>
+            <button className="btn btn-default border">Create Account</button>
         </form>
       </div>
     </div>
